1a79e9b37cf6e22de141f3b6a9a924171b308088
idakazuki <kazuki.fykgu@gmail.com>
2021-07-11 00:01:50 +0900

    モデル・カラムの日本語設定を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(email: "test@example.com", password: "password")
 puts "ユーザーの初期データインポートに成功しました。"
+
+# ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+# ActiveRecord::Base.connection.execute("TRUNCATE TABLE foods RESTART IDENTITY CASCADE")
+
+# user1 = User.create!(email: "satou@example.com", password: "password")
+# user2 = User.create!(email: "suzuki@example.com", password: "password")
+# user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+# Food.create!(content: "おはよう", user_id: user2.id)
+# Food.create!(content: "こんにちは", user_id: user3.id)
+# Food.create!(content: "こんばんは", user_id: user3.id)
+
+# puts "初期データの投入に成功しました！"
