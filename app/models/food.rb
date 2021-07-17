37b5ae97d1e2b73e77f5713f94f8989ef78501b9
idakazuki <kazuki.fykgu@gmail.com>
2021-07-18 08:39:46 +0900

    いいね機能用のルーティングとコントローラーを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,8 @@ class Food < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
   mount_uploader :image, ImageUploader
 end
